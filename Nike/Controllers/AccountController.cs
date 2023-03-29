04f8e1e06b5098c0c4927310e406a6368b37c27b
Trần Thanh Nhân <90546715+nhanne@users.noreply.github.com>
2023-03-29 20:12:37 +0700

    Upload Register
    Chức năng đăng ký

---
+++ Nike/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Nike.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nike.Controllers
+{
+    public class AccountController : Controller
+    {
+        private QuanLySanPhamEntities _db = new QuanLySanPhamEntities();
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        //POST: Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(KhachHang khachhang)
+        {
+            if (ModelState.IsValid)
+            {
+                var check = _db.KhachHangs.FirstOrDefault(s => s.Email == khachhang.Email);
+                if (check == null)
+                {
+                    _db.Configuration.ValidateOnSaveEnabled = false;
+                    _db.KhachHangs.Add(khachhang);
+                    _db.SaveChanges();
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    ViewBag.error = "Email already exists";
+                    return View();
+                }
+            }
+            return View();
+
+
+        }
+
+    }
+}

d974d4236c034b0a80bdc214fef37ea5bbe6a6ad
Trần Thanh Nhân <90546715+nhanne@users.noreply.github.com>
2023-04-18 19:37:33 +0700

    Task Thêm sản phẩm mới - Sprint 5
    Hoàn thành code thêm sản phẩm mới vào cửa hàng - sprint 5

--- Nike/Areas/Admin/Controllers/ProductController.cs
+++ Nike/Areas/Admin/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PagedList;
+using System.IO;
 
 namespace Nike.Areas.Admin.Controllers
 {
@@ -17,23 +18,70 @@ namespace Nike.Areas.Admin.Controllers
         {
             int pageNum = (page ?? 1);
             int pageSize = 10;
-
-            NhanVien nv = (NhanVien)Session["NV"];
-            if (nv.MaChucVu == 1)
-            {
-                return RedirectToAction("Index", "Home");
-            }
-            var products = (from s in _db.Products select s).ToList().ToPagedList(pageNum, pageSize);
+            var dsProduct = (from s in _db.Products select s).ToList().ToPagedList(pageNum, pageSize);
+            var totalPage = (double)dsProduct.Count();
+            ViewBag.totalPage = Math.Ceiling(totalPage);
+           
+            // Tìm kiêm sản phẩm - Duy
             if (!String.IsNullOrEmpty(searchString))
             {
                 searchString = searchString.ToLower();
-                ViewBag.products = _db.Products.Where(s => s.ProductName.ToLower().Contains(searchString) || s.Catalog.CatalogName.ToLower().Contains(searchString)).ToPagedList(pageNum, pageSize);
+                ViewBag.searchStr = searchString;
+
+                var dsSearch = _db.Products.ToList().Where(s => s.ProductName.ToLower().Contains(searchString) || s.Catalog.CatalogName.ToLower().Contains(searchString));
+                totalPage = (double)dsSearch.Count() / 10;
+                ViewBag.totalPage = Math.Ceiling(totalPage);
+                ViewBag.products = dsSearch.ToPagedList(pageNum, pageSize);
             }
+            //
             else
             {
-                ViewBag.products = products;
+                ViewBag.products = dsProduct;
             }
-            return View(products);
+            return View(dsProduct);
         }
+        // Tạo sản phẩm mới - Nhân
+        public ActionResult Create()
+        {
+            ViewBag.CatalogId = new SelectList(_db.Catalogs, "ID", "CatalogName");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(HttpPostedFileBase file, [Bind(Include = "CatalogId,ProductName,ProductCode,UnitPrice,SoLuong,ProductSold,ProductSale,PriceOld")] Product model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                String anh = "/Hinh/Product/hinhphone.jpg";
+                if (file != null)
+                {
+                    string pic = System.IO.Path.GetFileName(file.FileName);
+                    String path = System.IO.Path.Combine(
+                                           Server.MapPath("~/Hinh"), pic);
+                    file.SaveAs(path);
+                    anh = pic;
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        file.InputStream.CopyTo(ms);
+                        byte[] array = ms.GetBuffer();
+                    }
+                }
+                model.Picture = anh;
+                Random prCode = new Random();
+                model.ProductCode = String.Concat("PR", prCode.Next(5000, 7000).ToString());
+                model.ProductSold = 0;
+                model.UnitPrice = (model.ProductSale != null) ? (model.UnitPrice = (model.PriceOld - (model.PriceOld * int.Parse(model.ProductSale)) / 100)) : (model.UnitPrice = model.PriceOld);
+                _db.Products.Add(model);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CatalogId = new SelectList(_db.Catalogs, "ID", "CatalogName", model.CatalogId);
+            return View(model);
+        }
+
     }
+
+
+
 }

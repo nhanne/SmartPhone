38ccfe5bd594aab14a9a0bae91af6c7e6b209868
Thắng <vominhthanggcd@gmail.com>
2023-04-24 23:34:05 +0700

    Sprint 6 Xem danh sách đơn hàng
    Đã hoàn thành

---
+++ Nike/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,62 @@
+﻿using Nike.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nike.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private QuanLySanPhamEntities _db = new QuanLySanPhamEntities();
+        // GET: Admin/Order
+        public ActionResult Index(string sort)
+        {
+            Sort(sort);
+            return View();
+        }
+
+
+        public void Sort(string sort)
+        {
+            var orderList = (from s in _db.Orders select s).ToList();
+            foreach (var order in orderList)
+            {
+                if (order.NgayGiao < DateTime.Now && order.Status == "Đang giao hàng")
+                {
+                    Order editOrder = _db.Orders.Find(order.ID);
+                    editOrder.Status = "Hoàn thành";
+                    _db.Entry(editOrder).State = EntityState.Modified;
+                    _db.SaveChanges();
+                }
+            }
+            if (String.IsNullOrEmpty(sort))
+            {
+                ViewBag.orderList = orderList;
+            }
+            else
+            {
+                switch (sort)
+                {
+                    case "Wait":
+                        ViewBag.orderList = orderList.Where(s => s.Status == "Chưa giao hàng");
+                        break;
+                    case "Deli":
+                        ViewBag.orderList = orderList.Where(s => s.Status == "Đang giao hàng");
+                        break;
+                    case "Done":
+                        ViewBag.orderList = orderList.Where(s => s.Status == "Hoàn thành");
+                        break;
+                    case "Cancel":
+                        ViewBag.orderList = orderList.Where(s => s.Status == "Đã hủy");
+                        break;
+                    default:
+                        ViewBag.orderList = orderList.Where(s => s.KhachHang.Sdt.ToString().Contains(sort));
+                        break;
+                }
+            }
+        }
+    }
+}
